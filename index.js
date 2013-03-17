0ad14e0426422f56db5e781d78720d9d95352e33
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-17 08:40:05 +0000

    Use hyperquest instead of request

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var request = require('hyperquest');
 var zlib = require('zlib');
 var tar = require('tar');
 var promise = require('nodeify').Promise;
@@ -7,31 +7,41 @@ module.exports = download;
 function download(user, repo, tag, dir, callback) {
   return promise(function (res) {
     var url = 'https://github.com/' + user + '/' + repo + '/archive/' + tag + '.tar.gz'
-    var a = request(url);
-    var b = zlib.createGunzip();
-    var c = tar.Extract({
-      path: dir,
-      strip: 1
-    });
-    a.pipe(b).pipe(c);
+    getURL(url, function (ex, a) {
+      if (ex) return res.reject(ex);
+      var b = zlib.createGunzip();
+      var c = tar.Extract({
+        path: dir,
+        strip: 1
+      });
+      a.pipe(b).pipe(c);
 
-    a.on('response', response);
-    a.on('error', err);
-    b.on('error', err);
-    c.on('error', err);
-    c.on('end', done);
+      b.on('error', err);
+      c.on('error', err);
+      c.on('end', done);
 
-    function response(response) {
-      if (response.statusCode !== 200) {
-        res.reject(new Error('Server responeded with status code: ' + response.statusCode));
+      function err(err) {
+        console.warn('error');
+        res.reject(err);
       }
-    }
-    function err(err) {
-      console.warn('error');
-      res.reject(err);
-    }
-    function done() {
-      res.fulfill(dir);
-    }
+      function done() {
+        res.fulfill(dir);
+      }
+    });
   }).nodeify(callback);;
+}
+function getURL(url, cb) {
+  var stream = request(url);
+  stream.on('response', function response(response) {
+    if (response.statusCode === 302 || response.statusCode === 301) {
+      getURL(response.headers.location, cb);
+    } else if (response.statusCode !== 200) {
+      cb(new Error('Server responeded with status code ' + response.statusCode + ' for ' + user + '/' + repo + '/' + tag));
+    } else {
+      cb(null, stream);
+    }
+  });
+  stream.on('error', function (e) {
+    cb(e);
+  });
 }
